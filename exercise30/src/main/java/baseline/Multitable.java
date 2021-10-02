78d0d4d6c32dba786bfdd421e7dfdf986d2ae2b8
Eric Heston <eric.heston98@gmail.com>
2021-10-02 17:11:36 -0400

    first commit

---
+++ exercise30/src/main/java/baseline/Multitable.java
@@ -0,0 +1,13 @@
+package baseline;
+
+public class Multitable {
+    int oneThruWhat = 12;
+
+    public int getOneThruWhat() {
+        return oneThruWhat;
+    }
+
+    public void print(){
+        //generates table with double for loop
+    }
+}
---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,13 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Eric Heston
+ */
+package baseline;
+
+public class Solution30 {
+    public static void main(String[] args) {
+
+        Multitable twelve = new Multitable();
+        twelve.print();
+    }
+}
