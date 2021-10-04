6b6fb84bd89e3ec067f6e4819b32d859e4d10d1c
Eric Heston <eric.heston98@gmail.com>
2021-10-03 23:26:22 -0400

    first commit skeleton

---
+++ exercise38/src/main/java/baseline/Application.java
@@ -0,0 +1,43 @@
+package baseline;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Application {
+    //stores string of nums separated by space
+    private String userStringNums = "";
+
+    public String getUserStringNums() {
+        return userStringNums;
+    }
+    public void setUserString(String userString) {
+        this.userString = userString;
+    }
+
+    public void userString() {
+        //ask user to enter list of numbers separated by a space
+        //store this entire thing as the class variable userStringNums
+    }
+    public ArrayList makeArray() {
+        //turns string with white spaces into array
+        //returns array of nums
+
+        //adds number from String into array element until it finds a white space
+        //when it finds a white space it skips on char and then starts filling in next array element
+        ArrayList numberArray = new ArrayList<>();
+
+        return numberArray;
+    }
+
+    public void printEvenNumbers() {
+        getEvenNums();
+        //prints evenNums to user
+    }
+
+    private ArrayList getEvenNums() {
+        //finds the even numbers in the array
+        //creates a new array with only those nums
+        //returns even array
+        return null;
+    }
+}
---
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -0,0 +1,17 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Eric Heston
+ */
+
+package baseline;
+
+public class Solution38 {
+    public static void main(String[] args) {
+
+        Application application = new Application();
+        application.userString();
+        application.makeArray();
+        application.printEvenNumbers();
+    }
+}
+
